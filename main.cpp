003676dd38792ac00ec09ed7daefaa4d6fa4d2fc
LFGA-10 <braiselala@gmail.com>
2026-05-07 21:31:53 +0200

    Full project development: Implemented core Game Engine structure with SDL2 and CMake

--- main.cpp
+++ main.cpp
@@ -1,2 +1,32 @@
-#include <iostream>
-int main() { std::cout << "Starting cpp-game-engine-2d"; return 0; }
+#include "Game.hpp"
+
+Game* game = nullptr;
+
+int main(int argc, char* argv[]) {
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+
+    Uint32 frameStart;
+    int frameTime;
+
+    game = new Game();
+    game->init("Spark Engine 2D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+    while (game->running()) {
+        frameStart = SDL_GetTicks();
+
+        game->handleEvents();
+        game->update();
+        game->render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if (frameDelay > frameTime) {
+            SDL_Delay(frameDelay - frameTime);
+        }
+    }
+
+    game->clean();
+
+    return 0;
+}
